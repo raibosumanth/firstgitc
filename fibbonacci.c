090ac02d46e394b34b25616c8a65b02ea57f9e73
sumanthK638 <sumanthkumar638@gmail.com>
2021-09-30 12:50:08 +0530

    first c program

---
+++ fibbonacci.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    int a=0,b=1,c;
+    printf("enter the number\n");
+    scanf("%d",&n);
+    printf("\n the fibonacci series is\n");
+    for(i=0;i<=n;i++)
+    {
+        printf("%d ",a);
+        c=a+b;
+        a=b;
+        b=c;
+    }
+}
